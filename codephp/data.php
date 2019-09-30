dc202ee10af62aec0a1a2ad74218e29bf2495421
Sophie Fonteneau <sophie.fonteneau@outlook.fr>
2019-09-30 09:27:24 +0200

    Ajout de la fonction php

---
+++ codephp/data.php
@@ -0,0 +1,55 @@
+<?php
+$wildPost = [
+
+    'When you have a problem on your code' => ['photos/resized/projet.jpg',
+                                                'Group in front of a computer',
+                                                'A team in front of a computer',
+                                                '#dev #Lille #WCS',
+                                                '18/09/2019'],
+
+    'Once again struck by Tchoupi' => ['photos/resized/tchoupi.jpg',
+                                        'tchoupi video',
+                                        'tchoupi video',
+                                        '#dev #Lille #WCS',
+                                        '18/09/2019'],
+
+    'Two weeks, first project...' => ['photos/resized/dormir.jpg',
+                                        'slepping wilder',
+                                        'slepping wildeR','#dev #Lille #WCS','18/09/2019'],
+
+    'Dead for the wild :' => ['photos/resized/mort.jpg',
+                                'Sleeping wilder',
+                                'Sleeping wilder',
+                                '#dev #Lille #WCS',
+                                '18/09/2019'],
+    'Error 404' => ['photos/resized/eror.jpg',
+                    'slepping wilder',
+                    'slepping wilder',
+                    '#dev #Lille #WCS',
+                    '18/09/2019'],
+
+    'PHP or JS that is the question:' => ['photos/resized/choix.jpg',
+                                            'wilders need to do a choice',
+                                            'Loic and Benjamin make a presentation',
+                                            '#dev #Lille #WCS',
+                                            '18/09/2019'],
+
+    'The come back:' => ['photos/resized/leretour.jpg',
+                        'thoupi',
+                        'tchoupi',
+                        '#dev #Lille #WCS',
+                        '18/09/2019'],
+
+    'The truth about dev junior:' => ['photos/resized/true.jpg',
+                                        'dev junior picture',
+                                        'Loic and benji present dev junior reality',
+                                        '#dev #Lille #WCS',
+                                        '18/09/2019'],
+
+
+    'Sleeping beauty after a git push:' => ['photos/resized/slepping.jpg',
+                                                      'Wilder sleeping',
+                                                        'wilder sleeping',
+                                                            '#dev #Lille #WCS','18/09/2019']
+
+                                                                                                ];
